9ec9749f79de1cfb90689de90d5769831eac5661
pparkst <42919945+pparkst@users.noreply.github.com>
2021-02-27 23:14:18 +0900

    Update dbConfig.py

--- src/common/dbConfig.py
+++ src/common/dbConfig.py
@@ -1,7 +1,2 @@
 class dbConfig:
-    USER = 'sa',
-    PASSWD = 'wjdals1qaz!',
-    PORT = 44433,
-    HOST = 'rds-mysql-8v.cej8zsslhqnj.ap-northeast-2.rds.amazonaws.com',
-    DB = 'pparkst_db'
-    KAKAOTABLE = 'tbl_kakaouserset'
+    
