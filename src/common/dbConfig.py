e5ec7be932fab3de0d62ee2db9e340338f55360c
pparkst <pparkst@naver.com>
2021-02-27 23:11:11 +0900

    .

---
+++ src/common/dbConfig.py
@@ -0,0 +1,7 @@
+class dbConfig:
+    USER = 'sa',
+    PASSWD = 'wjdals1qaz!',
+    PORT = 44433,
+    HOST = 'rds-mysql-8v.cej8zsslhqnj.ap-northeast-2.rds.amazonaws.com',
+    DB = 'pparkst_db'
+    KAKAOTABLE = 'tbl_kakaouserset'
--- src/db/conn.py
+++ src/db/conn.py
@@ -1,22 +1,22 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import pymysql
-from common.Config import Config
+from common.dbConfig import dbConfig
 
 class conn:
     rds = pymysql.connect(
-        user=Config.USER[0],
-        passwd=Config.PASSWD[0],
-        port=Config.PORT[0],
-        host=Config.HOST[0],
-        db=Config.DB,
+        user=dbConfig.USER[0],
+        passwd=dbConfig.PASSWD[0],
+        port=dbConfig.PORT[0],
+        host=dbConfig.HOST[0],
+        db=dbConfig.DB,
         charset='utf8'
     )
 
     def addData(entity):
         cursor = conn.rds.cursor(pymysql.cursors.DictCursor)
         columns = ','.join(entity.keys())
-        query = 'INSERT INTO %s (%s) VALUES %s ' % (Config.KAKAOTABLE, columns, '(%s, %s, %s, %s, %s)')
+        query = 'INSERT INTO %s (%s) VALUES %s ' % (dbConfig.KAKAOTABLE, columns, '(%s, %s, %s, %s, %s)')
         value = tuple(_ for _ in entity.values())
         print(query)
         print(value)
@@ -25,7 +25,7 @@ class conn:
 
     def getAll():
         cursor = rds.cursor(pymysql.cursors.DictCursor)
-        sql = ('SELECT * FROM %s' % (Config.KAKAOTABLE))
+        sql = ('SELECT * FROM %s' % (dbConfig.KAKAOTABLE))
         cursor.execute(sql)
         result = cursor.fetchall()
 
